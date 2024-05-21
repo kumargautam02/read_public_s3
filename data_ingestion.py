981907577721356dee6c487bc4dba18f98fb76c6
Gautam <gautam.kumar410305@gmail.com>
2024-05-22 00:35:43 +0530

    adding data_ingestion.py

--- data_ingestion.py
+++ data_ingestion.py
@@ -2,4 +2,20 @@
 import s3fs
 s3 = s3fs.S3FileSystem(anon=True)
 
-s3.ls('s3://datasci-assignment/click_log/2024/05/10/00')
+
+
+def ingest_data_from_s3(current_working, s3, s3_path):
+    """
+    This function is used to Ingest data from Public S3 bucket site using s3fs library and store inside Landing Folder of Current working Directory. 
+    Parameters:
+    current_working: receive the current working directory to save data ex:- current_working_directory/Landing
+    s3: S3 object for Public s3_bucket.
+    s3_path: s3 bucket path from which we have to ingest data. 
+
+    returns: None
+    """
+
+    # current_working = os.getcwd()
+    # print(current_working)
+    s3 = s3fs.S3FileSystem(anon =  True)
+    s3.get(f'{s3_path}', f'{current_working}/Landing1/',recursive=True, maxdepth=None)
