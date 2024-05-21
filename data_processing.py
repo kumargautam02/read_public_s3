ece8777f14e7bfa2e45b668beda8ae2d004a25ac
Gautam <gautam.kumar410305@gmail.com>
2024-05-22 04:56:06 +0530

    trying to dynamically figure json scehma

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,11 @@
 import re
 import os
+import logging
 import s3fs
 from data_ingestion import ingest_data_from_s3
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+
 
 
 
@@ -20,14 +22,14 @@ print(str(os.getcwd()).replace("\\", "/"))
 
 
 
-df = spark.read.option("InferSchema", "True").option("mode", "PERMISSIVE").option("multiLine", True).json("C:/Users/Admin/Downloads/read_public_s3/Landing/click_log/2024/05/10/00/")
-df = df.select('*', "ip_geo.*", "query.*").drop("query", "ip_geo")
-# df.write.format("parquet")
-df = df.withColumn("real_filepath", input_file_name())
+df = spark.read.option("InferSchema", "True").option("mode", "PERMISSIVE").option("multiLine", True).json("C:/Users/Admin/Downloads/read_public_s3/Landing/postback-log/2024/05/10/00/performance_postback_log-1-2024-05-10-00-00-33-efc64b50-edbc-4a15-97ae-0aaefa93ce79")
+# df = df.select('*', "ip_geo.*", "query.*").drop("query", "ip_geo")
+# # df.write.format("parquet")
+# df = df.withColumn("real_filepath", input_file_name())
 
-df = df.withColumn("actual_file" , split(df.real_filepath, '/',limit=-1))
-df = df.withColumn("count_file", size(df.actual_file))
-df = df.withColumn("actual_file" , df.actual_file[col("count_file")-1]).drop("count_file")
-# df.withColum("filepath", F.regexp_extract("filepath", "State=(.+)\.snappy\.parquet", 1)
+# df = df.withColumn("actual_file" , split(df.real_filepath, '/',limit=-1))
+# df = df.withColumn("count_file", size(df.actual_file))
+# df = df.withColumn("actual_file" , df.actual_file[col("count_file")-1]).drop("count_file")
+# # df.withColum("filepath", F.regexp_extract("filepath", "State=(.+)\.snappy\.parquet", 1)
 
 df.show()
